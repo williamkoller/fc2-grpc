7abc21b2c21bd74649c7ff060c813b95c7f9ccbc
William Koller <williamkoller404@gmail.com>
2022-05-30 11:35:20 -0300

    feat: add AddUserStreamBoth in user_service

--- services/user_service.go
+++ services/user_service.go
@@ -94,3 +94,26 @@ func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
 		fmt.Println("Adding", req.GetName())
 	}
 }
+
+func (*UserService) AddUserStreamBoth(stream pb.UserService_AddUserStreamBothServer) error {
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			log.Fatalf("Error receiving stream from the client: %v", err)
+		}
+
+		err = stream.Send(&pb.UserResultStream{
+			Status: "Added",
+			User: req,
+		})
+
+		if err != nil {
+			log.Fatalf("Error send stream to the client: %v", err)
+		}
+	}
+}
